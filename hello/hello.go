569c64739697663ae831243399bb09e6863e4b4c
K0966149 <vatcharit.o@kbtg.tech>
2023-10-31 00:43:55 +0700

    adjust time to 3 second.

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,11 @@
 package main
 
-import ("fmt" 
-		"time"
-		)
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
-	time.Sleep(2*time.Second)
+	time.Sleep(3 * time.Second)
 }
